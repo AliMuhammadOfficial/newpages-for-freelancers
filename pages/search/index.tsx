b882ee176c90a499cd209b88e6f463202c41c842
Ali Muhammad <ali9796408@gmail.com>
2024-08-01 00:34:11 +0500

    edge run time to search

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,3 +1,5 @@
+export const runtime = "experimental-edge";
+
 import { House } from "@/interfaces/interfaces";
 import MainServerComponent from "@/components/reusable/MainServerComponent";
 import { fetchResults } from "../../kerkesa/fetchResults";
