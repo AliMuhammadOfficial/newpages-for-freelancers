3acaf74f9a5ae0226c304eaf99b60b09c5851461
Ali Muhammad <ali9796408@gmail.com>
2024-08-01 00:08:59 +0500

    depencies updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
+const nextConfig = (phase, { defaultConfig }) => {
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+          reactStrictMode: true,
+        }
+      }
+    
+      // Production config
+      return {
+        swcMinify: true,
+        images: {
+          loader: 'custom',
+          loaderFile: './cfImageLoader.js'
+        },
+      }
+}
+export default nextConfig
